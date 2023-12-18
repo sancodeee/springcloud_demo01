83cd66cf2a86942ca4f52236efae610f3b53240a
wangsen_a <wangsen_a@aspirecn.com>
2023-12-18 18:14:19 +0800

    gateway网关配置

--- gateway_demo/src/main/java/com/ws/config/AuthorizeFilter.java
+++ gateway_demo/src/main/java/com/ws/config/AuthorizeFilter.java
@@ -1,14 +1,23 @@
 package com.ws.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ws.common.Result;
+import lombok.experimental.Accessors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 /**
  * 网关全局过滤器
@@ -17,6 +26,7 @@ import reactor.core.publisher.Mono;
  * @date 2023/12/18
  */
 @Order(-1)
+@Accessors(chain = true)
 @Component
 @Slf4j
 public class AuthorizeFilter implements GlobalFilter {
@@ -25,18 +35,25 @@ public class AuthorizeFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        // 请求头中的token
+        String headerToken = headers.getFirst(X_ACCESS_TOKEN);
         // 获取请求参数
         MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
         // 获取token验证
-        String first = queryParams.getFirst(X_ACCESS_TOKEN);
-        log.info("X-Access-Token值：{}", first);
-        if ("token".equals(first)) {
-            // 放行
+        String token = queryParams.getFirst(X_ACCESS_TOKEN);
+        log.info("X-Access-Token值：{}", token);
+        log.info("Header中X-Access-Token值：{}", token);
+        if ("token".equals(token) || "token".equals(headerToken)) {
+            // 放行：继续执行过滤器链
             return chain.filter(exchange);
         }
         // 禁止访问
         exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-        // 结束处理
+        log.error("请求参数中token验证不通过");
+        // 返回不同过验证的请求
         return exchange.getResponse().setComplete();
     }
+
+
 }
