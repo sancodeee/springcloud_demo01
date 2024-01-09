1e03f5cd50bba8cffd0b5b512b7c129cc41b1cfe
sancodeee <wangsen715@gmail.com>
2024-01-09 23:09:52 +0800

    user实体类更新

--- service-authentication/src/main/java/com/ws/dao/UserMapper.java
+++ service-authentication/src/main/java/com/ws/dao/UserMapper.java
@@ -4,6 +4,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ws.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 用户映射器
+ *
+ * @author wangsen
+ * @date 2024/01/09
+ */
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
