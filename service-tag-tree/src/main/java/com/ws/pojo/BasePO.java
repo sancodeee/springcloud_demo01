c1b1e03e47430544f8a437497a156fcf1472d80a
sancodeee <wangsen715@gmail.com>
2024-01-11 00:09:17 +0800

    aop切面

---
+++ service-tag-tree/src/main/java/com/ws/pojo/BasePO.java
@@ -0,0 +1,48 @@
+package com.ws.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+/**
+ * 基础po
+ *
+ * @author wangsen
+ * @date 2024/01/10
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BasePO {
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
+
+    /**
+     * 创建者
+     */
+    private String creator;
+
+    /**
+     * 更新时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updateTime;
+
+    /**
+     * 更新者
+     */
+    private String updater;
+
+    /**
+     * 逻辑删除：0-未删除，1-已删除
+     */
+    private Integer isDeleted;
+
+}
