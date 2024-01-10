920ce6e8f12aedb581bb27e5f6f0b85cc25ac7c9
wangsen_a <wangsen_a@aspirecn.com>
2024-01-10 17:18:03 +0800

    aop

--- service-tag-tree/src/main/java/com/ws/TagTreeApplication.java
+++ service-tag-tree/src/main/java/com/ws/TagTreeApplication.java
@@ -1,11 +1,9 @@
 package com.ws;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.ws.dao")
 public class TagTreeApplication {
     public static void main(String[] args) {
         SpringApplication.run(TagTreeApplication.class, args);
