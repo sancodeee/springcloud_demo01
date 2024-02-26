4f8d64a32a9f6f20cfcf6696e800ae223b1e32c1
wangsen_a <wangsen_a@aspirecn.com>
2024-02-26 11:36:20 +0800

    gx

--- project-gateway/src/main/java/com/ws/GateWayApplication.java
+++ project-gateway/src/main/java/com/ws/GateWayApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GateWayApplicaton {
+public class GateWayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GateWayApplicaton.class, args);
+        SpringApplication.run(GateWayApplication.class, args);
     }
 }
